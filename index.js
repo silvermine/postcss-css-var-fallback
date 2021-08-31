62edc48b6d09ee102690e8d24db6a3f1e0a97ddc
Paul Bredenberg <paulbredenberg@gmail.com>
2021-08-31 11:06:36 -0400

    chore: initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+module.exports = (opts = { }) => {
+
+  // Work with options here
+
+  console.log('What are my opts?', opts);
+
+  return {
+    postcssPlugin: 'postcss-var-fallback',
+    /*
+    Root (root, postcss) {
+      // Transform CSS AST here
+    }
+    */
+
+    /*
+    Declaration (decl, postcss) {
+      // The faster way to find Declaration node
+    }
+    */
+
+    /*
+    Declaration: {
+      color: (decl, postcss) {
+        // The fastest way find Declaration node if you know property name
+      }
+    }
+    */
+  }
+}
+module.exports.postcss = true
